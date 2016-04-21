59b42599b169ac7c21921224c2e566c608cbd639
Daniel Lu <ludg@bc.edu>
2016-04-21 11:28:00 -0400

    Update MeasurePopulate.sql

--- MeasurePopulate.sql
+++ MeasurePopulate.sql
@@ -1,5 +1,5 @@
-INSERT INTO A_Measure (m_name) VALUES ('Cup');
-INSERT INTO A_Measure (m_name) VALUES ('Teaspoon');
-INSERT INTO A_Measure (m_name) VALUES ('Tablespoon');
-INSERT INTO A_Measure (m_name) VALUES ('Ounce');
-INSERT INTO A_Measure (m_name) VALUES ('Pound');
+INSERT INTO A_Measure (id,m_name) VALUES (1,'Cup');
+INSERT INTO A_Measure (id,m_name) VALUES (2,'Teaspoon');
+INSERT INTO A_Measure (id,m_name) VALUES (3,'Tablespoon');
+INSERT INTO A_Measure (id,m_name) VALUES (4,'Ounce');
+INSERT INTO A_Measure (id,m_name) VALUES (5,'Pound');
