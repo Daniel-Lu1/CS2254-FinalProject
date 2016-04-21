81769a58d1e914961a9d43cb54f5d111c13a9757
Daniel Lu <ludg@bc.edu>
2016-04-21 00:22:20 -0400

    Update RIPopulate.sql

--- RIPopulate.sql
+++ RIPopulate.sql
@@ -8,13 +8,14 @@ INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,6,NULL,10
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,8,4,3);
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,9,3,3);
 
-
+//Ingredients and Amounts for Burgers
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,1,NULL,1);
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,2,NULL,1);
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,3,NULL,1);
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,4,NULL,1);
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,11,NULL,10);
 
+//Ingredients and Amounts for Quesadillas
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (3,3,5,1);
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (3,4,5,1);
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (3,5,5,1);
