260c16900a2b190b1c82b06f92663bd06cad5883
Nick Borisenko <borisenk@bc.edu>
2016-04-20 23:50:11 -0400

    MeasurePopulate.sql

---
+++ MeasurePopulate.sql
@@ -0,0 +1,5 @@
+INSERT INTO A_Measure (m_name) VALUES ('Cup');
+INSERT INTO A_Measure (m_name) VALUES ('Teaspoon');
+INSERT INTO A_Measure (m_name) VALUES ('Tablespoon');
+INSERT INTO A_Measure (m_name) VALUES ('Ounce');
+INSERT INTO A_Measure (m_name) VALUES ('Pound');
