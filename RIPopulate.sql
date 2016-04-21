745ef51fe8b0e6082773d8b95fc294df416e4a82
Nick Borisenko <borisenk@bc.edu>
2016-04-20 23:59:14 -0400

    Create RIPopulate.sql

---
+++ RIPopulate.sql
@@ -0,0 +1,14 @@
+INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,1,NULL,1);
+INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,2,NULL,2);
+INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,3,1,1);
+INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,4,5,1);
+INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,5,5,1);
+INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,6,NULL,10);
+INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,8,4,3);
+INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,9,3,3);
+
+INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,1,NULL,1);
+INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,2,NULL,1);
+INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,3,NULL,1);
+INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,4,NULL,1);
+INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,11,NULL,10);
