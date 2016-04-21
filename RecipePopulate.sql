48baf339ca8db430b8592479072973a25495c06e
Daniel Lu <ludg@bc.edu>
2016-04-21 11:19:22 -0400

    Update RecipePopulate.sql

--- RecipePopulate.sql
+++ RecipePopulate.sql
@@ -1,5 +1,5 @@
-INSERT INTO A_Recipe (r_name, url) VALUES ('Tacos','www.tacos.com');
-INSERT INTO A_Recipe (r_name, url) VALUES ('Burgers','www.burgers.com');
-INSERT INTO A_Recipe (r_name, url) VALUES ('Quesadillas','www.quesadillas.com');
-INSERT INTO A_Recipe (r_name, url) VALUES ('Enchilladas','www.enchilladas.com');
-INSERT INTO A_Recipe (r_name, url) VALUES ('Fajitas','www.fajitas.com');
+INSERT INTO A_Recipe (id, r_name, url) VALUES (1,'Tacos','www.tacos.com');
+INSERT INTO A_Recipe (id, r_name, url) VALUES (1,'Burgers','www.burgers.com');
+INSERT INTO A_Recipe (id, r_name, url) VALUES (1,'Quesadillas','www.quesadillas.com');
+INSERT INTO A_Recipe (id, r_name, url) VALUES (1,'Enchilladas','www.enchilladas.com');
+INSERT INTO A_Recipe (id, r_name, url) VALUES (1,'Fajitas','www.fajitas.com');
