1d6bdbeb9dbbdcd32f43fe1eabd8bcf7e0116b64
Nick Borisenko <borisenk@bc.edu>
2016-04-20 23:40:56 -0400

    RecipePopulate

---
+++ RecipePopulate.sql
@@ -0,0 +1,5 @@
+INSERT INTO A_Recipe (r_name, url) VALUES ('Tacos','www.tacos.com');
+INSERT INTO A_Recipe (r_name, url) VALUES ('Burgers','www.burgers.com');
+INSERT INTO A_Recipe (r_name, url) VALUES ('Quesadillas','www.quesadillas.com');
+INSERT INTO A_Recipe (r_name, url) VALUES ('Enchilladas','www.enchilladas.com');
+INSERT INTO A_Recipe (r_name, url) VALUES ('Fajitas','www.fajitas.com');
