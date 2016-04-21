2d8330614260e02ed361a606c86c402eee1a4750
Daniel Lu <ludg@bc.edu>
2016-04-21 00:10:40 -0400

    Update RIPopulate.sql

--- RIPopulate.sql
+++ RIPopulate.sql
@@ -8,14 +8,12 @@ INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,6,NULL,10
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,8,4,3);
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (1,9,3,3);
 
+
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,1,NULL,1);
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,2,NULL,1);
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,3,NULL,1);
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,4,NULL,1);
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (2,11,NULL,10);
-
-
-
 
 //Ingredients and amounts for Fajitas
 INSERT INTO A_RI (recipe_id,ingredient_id,measure_id,amount) VALUES (5,5,5,0.5);
