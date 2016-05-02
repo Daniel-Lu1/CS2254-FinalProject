0bbf60f796153dd110f61d1048f6902415ca91ae
Daniel Lu <ludg@bc.edu>
2016-05-01 23:36:58 -0400

    fixed logins

--- login.php
+++ login.php
@@ -1,12 +1,13 @@
 <?php
+include("dbconn.php");
 $dbc = connect_to_db("borisenk");
 $username=$_POST['email']; 
 $password=$_POST['password']; 
 
-$username = stripslashes($username);
+/*$username = stripslashes($username);
 $password = stripslashes($password);
 $username = mysql_real_escape_string($username);
-$password = mysql_real_escape_string($password);
+$password = mysql_real_escape_string($password);*/
 
 $query = "SELECT * FROM `Chef` WHERE `email` = '".$username."'";
 $result = perform_query($dbc, $query);
@@ -14,12 +15,12 @@ if (mysqli_num_rows($result)==1){
 session_start();
     $_SESSION['loggedin'] = true;
     $_SESSION['username'] = $username;
-    header("Location: cscilab.bc.edu/~borisenk/finalproject/index.html");
+    header("Location: index.html");
 	exit();
 }
 else{
-echo '<h1>Incorrect Login Information</h1>'
-echo '<a href="http://cscilab.bc.edu/~borisenk/finalproject/login.html">Click here to try again.</a>';
+echo '<h1>Incorrect Login Information</h1>';
+echo '<a href="http://cscilab.bc.edu/~ludg/finalproject/login.html">Click here to try again.</a>';
 }
 /*In PHP file with the table displayed, 
 session_start();
@@ -29,4 +30,5 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
     echo normal table without member features. 
 }*/
 ?>
+
 
