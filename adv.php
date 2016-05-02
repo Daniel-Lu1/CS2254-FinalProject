8e8f0ef5e059d70072fb583eb11a80b54d341700
Nick <nickboris48@yahoo.com>
2016-05-02 01:11:51 -0400

    Advanced Search Functionality Added

---
+++ adv.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<form method="get" action="results.php">
+		<label>Keyword(s): </label><input type="text" name="key" placeholder="Enter a keyword"><br>
+		<label>Number of Results: </label><input type="text" name="num" placeholder="Enter a maximum number of results to show"><br>
+		<label>Select a diet label: </label><br>
+			<div class="diets">
+				<input type="radio" name="diet" value="balanced">Balanced<br>
+				<input type="radio" name="diet" value="high-protein">High-Protein<br>
+				<input type="radio" name="diet" value="high-fiber">High-Fiber<br>
+				<input type="radio" name="diet" value="low-fat">Low-Fat<br>
+				<input type="radio" name="diet" value="low-carb">Low-Carb<br>
+				<input type="radio" name="diet" value="low-sodium">Low-Sodium<br>
+			</div>
+		<label>Select a health label:</label><br>
+			<div class="healths">
+				<input type="radio" name="health" value="vegan">Vegan<br>
+				<input type="radio" name="health" value="vegetarian">Vegetarian<br>
+				<input type="radio" name="health" value="paleo">Paleo<br>
+				<input type="radio" name="health" value="diary-free">Dairy-Free<br>
+				<input type="radio" name="health" value="gluten-free">Gluten-Free<br>
+				<input type="radio" name="health" value="wheat-free">Wheat-Free<br>
+				<input type="radio" name="health" value="fat-free">Fat-Free<br>
+				<input type="radio" name="health" value="low-sugar">Low-Sugar<br>
+				<input type="radio" name="health" value="egg-free">Egg-Free<br>
+				<input type="radio" name="health" value="peanut-free">Peanut-Free<br>
+				<input type="radio" name="health" value="tree-nut-free">Tree-nut-free<br>
+				<input type="radio" name="health" value="soy-free">Soy-Free<br>
+				<input type="radio" name="health" value="fish-free">Fish-Free<br>
+				<input type="radio" name="health" value="shellfish-free">Shellfish-Free<br>
+			</div>
+		<br>
+		<label>Calorie Range: </label><br>
+			<label>Low</label><input type="text" name="low" placeholder="0"><br>
+			<label>High</label><input type="text" name="high" placeholder="10000"><br>
+		<input type="submit" name="advsub" value="Search">
+	</form>
+</body>
+</html>
--- results.php
+++ results.php
@@ -11,6 +11,9 @@
 					if(isset($_GET['submit'])){ 
 						echo 'value="'.$_GET['name'].'"';
 					} 
+					if(isset($_GET['advsub'])){
+						echo 'value="'.$_GET['key'].'"';
+					}
 				?>
 			>
 			<input type="submit" name="submit" id="submit" value="Search">
@@ -22,10 +25,10 @@
 	<script>
 		$(document).ready(function(){
 			$("#submit").click(function(){
-				search();
+				simple();
 			})
 		});
-		function search(){
+		function simple(){
 			var request = $.ajax({
 				url: "https://api.edamam.com/search",
 				jsonp: "callback",
@@ -35,7 +38,6 @@
 					//from:0,
 					//to:100,
 					//calories: "gte 250, lte 500",
-					//diet: "balanced",
 					api_id:  "61496f24",
 					api_key: "df682debbdd8c22dd3776834d4b83a41"
 				},
@@ -61,12 +63,195 @@
 					};
 					$("#results").append("</tbody>");
 				}
-			});
+			});			
+		};
+		function adv(){
+			var $_GET = <?php echo json_encode($_GET); ?>;
+			var key = $_GET['key'];
+			if(!$_GET['num']){
+				var num = 10;
+			}else{
+				var num = $_GET['num'];
+			}
+			if(!$_GET['diet']){
+				var diet = null;
+			}else{
+				var diet = $_GET['diet'];
+			}
+			if(!$_GET['health']){
+				var health = null;
+			}else{
+				var health = $_GET['health'];
+			}
+			if(!$_GET['low']){
+				var low = 0;
+			}else{
+				var low = $_GET['low'];
+			}
+			if(!$_GET['high']){
+				var high = 100000;
+			}else{
+				var high = $_GET['high'];
+			}
+			var cal = "gte " + low + "," + "lte " + high;
+			console.log($_GET['key']+" "+$_GET['num']+" "+$_GET['diet']+" "+$_GET['health']+" "+$_GET['low']+" "+$_GET['high']+" "+cal);
+			if(diet!=null && health!=null){
+				var request = $.ajax({
+					url: "https://api.edamam.com/search",
+					jsonp: "callback",
+					dataType: "jsonp",
+					data: {
+						q: key,
+						from: 0,
+						to: num,
+						diet: diet,
+						health: health,
+						calories: cal,
+						api_id:  "61496f24",
+						api_key: "df682debbdd8c22dd3776834d4b83a41"
+					},
+					success: function(callback){
+						$("#results").html("");
+						$("#results").append("<thead><tr><th>Recipe Name</th><th>Calories</th><th>Ingredients</th></tr></thead><tbody>");
+						var len = callback.hits.length;
+						for(i=0;i<len;i++){
+							var str1 = "<tr><td>"+callback.hits[i].recipe.label+"</td><td>"+callback.hits[i].recipe.calories+"</td><td><ul>";
+							var len2 = callback.hits[i].recipe.ingredients.length;
+							var s = "<li>";
+							var c = "</li>";
+							var str2 = "";
+							for(j=0;j<len2;j++){
+								var line = callback.hits[i].recipe.ingredients[j].text;
+								var st = s.concat(line);
+								var fin = st.concat(c);
+								str2 = str2.concat(fin);
+							};
+							var str3 = "</ul></td></tr>";
+							var str = str1.concat(str2,str3);
+							$("#results").append(str);
+						};
+						$("#results").append("</tbody>");
+					}
+				});
+			}else if(diet==null && health!=null){
+				var request = $.ajax({
+					url: "https://api.edamam.com/search",
+					jsonp: "callback",
+					dataType: "jsonp",
+					data: {
+						q: key,
+						from: 0,
+						to: num,
+						health: health,
+						calories: cal,
+						api_id:  "61496f24",
+						api_key: "df682debbdd8c22dd3776834d4b83a41"
+					},
+					success: function(callback){
+						$("#results").html("");
+						$("#results").append("<thead><tr><th>Recipe Name</th><th>Calories</th><th>Ingredients</th></tr></thead><tbody>");
+						var len = callback.hits.length;
+						for(i=0;i<len;i++){
+							var str1 = "<tr><td>"+callback.hits[i].recipe.label+"</td><td>"+callback.hits[i].recipe.calories+"</td><td><ul>";
+							var len2 = callback.hits[i].recipe.ingredients.length;
+							var s = "<li>";
+							var c = "</li>";
+							var str2 = "";
+							for(j=0;j<len2;j++){
+								var line = callback.hits[i].recipe.ingredients[j].text;
+								var st = s.concat(line);
+								var fin = st.concat(c);
+								str2 = str2.concat(fin);
+							};
+							var str3 = "</ul></td></tr>";
+							var str = str1.concat(str2,str3);
+							$("#results").append(str);
+						};
+						$("#results").append("</tbody>");
+					}
+				});
+			}else if(diet!=null && health==null){
+				var request = $.ajax({
+					url: "https://api.edamam.com/search",
+					jsonp: "callback",
+					dataType: "jsonp",
+					data: {
+						q: key,
+						from: 0,
+						to: num,
+						diet: diet,
+						calories: cal,
+						api_id:  "61496f24",
+						api_key: "df682debbdd8c22dd3776834d4b83a41"
+					},
+					success: function(callback){
+						$("#results").html("");
+						$("#results").append("<thead><tr><th>Recipe Name</th><th>Calories</th><th>Ingredients</th></tr></thead><tbody>");
+						var len = callback.hits.length;
+						for(i=0;i<len;i++){
+							var str1 = "<tr><td>"+callback.hits[i].recipe.label+"</td><td>"+callback.hits[i].recipe.calories+"</td><td><ul>";
+							var len2 = callback.hits[i].recipe.ingredients.length;
+							var s = "<li>";
+							var c = "</li>";
+							var str2 = "";
+							for(j=0;j<len2;j++){
+								var line = callback.hits[i].recipe.ingredients[j].text;
+								var st = s.concat(line);
+								var fin = st.concat(c);
+								str2 = str2.concat(fin);
+							};
+							var str3 = "</ul></td></tr>";
+							var str = str1.concat(str2,str3);
+							$("#results").append(str);
+						};
+						$("#results").append("</tbody>");
+					}
+				});
+			}else{
+				var request = $.ajax({
+					url: "https://api.edamam.com/search",
+					jsonp: "callback",
+					dataType: "jsonp",
+					data: {
+						q: key,
+						from: 0,
+						to: num,
+						calories: cal,
+						api_id:  "61496f24",
+						api_key: "df682debbdd8c22dd3776834d4b83a41"
+					},
+					success: function(callback){
+						$("#results").html("");
+						$("#results").append("<thead><tr><th>Recipe Name</th><th>Calories</th><th>Ingredients</th></tr></thead><tbody>");
+						var len = callback.hits.length;
+						for(i=0;i<len;i++){
+							var str1 = "<tr><td>"+callback.hits[i].recipe.label+"</td><td>"+callback.hits[i].recipe.calories+"</td><td><ul>";
+							var len2 = callback.hits[i].recipe.ingredients.length;
+							var s = "<li>";
+							var c = "</li>";
+							var str2 = "";
+							for(j=0;j<len2;j++){
+								var line = callback.hits[i].recipe.ingredients[j].text;
+								var st = s.concat(line);
+								var fin = st.concat(c);
+								str2 = str2.concat(fin);
+							};
+							var str3 = "</ul></td></tr>";
+							var str = str1.concat(str2,str3);
+							$("#results").append(str);
+						};
+						$("#results").append("</tbody>");
+					}
+				});
+			}
 		};
 	</script>
 	<?php
 		if(isset($_GET['submit'])) { ?>
-			<script>search();</script>
+			<script>simple();</script>
+	<?php }
+		if(isset($_GET['advsub'])) { ?>
+			<script>adv();</script>
 	<?php } ?>
 </body>
 </html>
