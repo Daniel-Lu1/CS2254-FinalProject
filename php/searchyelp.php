2f5d19fed79b55ead2a5a8070fe2b8ae3a0e956a
Daniel Lu <ludg@bc.edu>
2016-05-05 00:23:11 -0400

    yelp page

---
+++ php/searchyelp.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+		<form method="post" id="yelp" action="yelp.php">
+				Find: <input type="text" name="food" id="foodtype" placeholder="pizza, take out, El Pelon" required>
+				Near: <input type="text" name="location" placeholder="Boston College, MA" required>
+				<input type="submit" name="submit" value="Go!">
+		</form>
+</body>
+</html>
