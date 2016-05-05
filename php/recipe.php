43f51f301494580850d0a84781dc9b641151b172
Eli Waalkes <eliwaalkes1@gmail.com>
2016-05-05 00:20:50 -0400

    updated

--- php/recipe.php
+++ php/recipe.php
@@ -1,23 +1,46 @@
 <?php
 session_start();
 ?>
+
 <!DOCTYPE php>
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
 	<title>The Cookbook</title>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
+<div id="recipeBody">
 <nav class="navbar navbar-default">
 	  <div class="container-fluid">
 	    <!-- Brand and toggle get grouped for better mobile display -->
-	    <!--
 	    <div class="navbar-header">
 	      <a class="navbar-brand" href="http://cscilab.bc.edu/~borisenk/test/index.php">Cookbook</a>
 	    </div>
-		-->
-
+		<form class="navbar-form navbar-left" role="search" method="get" id="search" action="results.php">
+          <input type="text" class="form-control" id="name" name="name" placeholder="Enter a keyword"
+          	<?php 
+				if(isset($_GET['submit'])){ 
+					echo 'value="'.$_GET['name'].'"';
+				} 
+				if(isset($_GET['advsub'])){
+					echo 'value="'.$_GET['key'].'"';
+				}
+			?>
+          required>
+          <input type="submit" class="btn btn-default" name="submit" id="submit" value="Search">
+          <button onclick="location.href ='adv.php'" type="button" class="btn btn-default">Advanced Search</button>
+    	</form>
 	    <!-- Collect the nav links, forms, and other content for toggling -->
 	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 	      <?php
@@ -25,10 +48,12 @@ session_start();
 					if($_SESSION['username'] == 'chefsidekick@yahoo.com'){
 					echo '<button class="btn btn-default navbar-btn navbar-nav navbar-right" onclick="location.href = \'admin2.php\';" id="adminpage">Admin Page</button>';
 					echo '<button class="btn btn-default navbar-btn navbar-nav navbar-right" onclick="location.href = \'displayrecipes.php\';" id="display">Show Saved Recipes</button>';
+
 					echo '<button class="btn btn-default navbar-btn navbar-nav navbar-right" onclick="location.href = \'logout.php\';" id="logout">Log out</button>';
 					}
 					else{
 					echo '<button class="btn btn-default navbar-btn navbar-nav navbar-right" onclick="location.href = \'http://cscilab.bc.edu/~waalkes/cooking/displayRecipes.php\';" id="display">Show Saved Recipes</button>';
+
 					echo '<button class="btn btn-default navbar-btn navbar-nav navbar-right" onclick="location.href = \'logout.php\';" id="logout">Log out</button>';
 					}
 				}
@@ -41,7 +66,7 @@ session_start();
 	    </div><!-- /.navbar-collapse -->
 	  </div><!-- /.container-fluid -->
 	</nav>
-	
+<center>
 <div id="recipePhoto" name="recipePhoto">
 	<div class="inner first"></div>
 	<div class="inner second"></div>
@@ -49,21 +74,18 @@ session_start();
 	<div class="inner fourth"></div>
 	<div class="inner fifth"></div>
 </div>
-<br>
-<?php
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
-echo '
+<div id="sixth"></div>
 <form method="post" action="saveRecipe.php" id="saveRecipeForm">
-	<input type="submit" name="saveRecipeButton" id="saveRecipeButton" value="Save this Recipe!">
-	<input type="hidden" name="recipeNameHidden" id="recipeNameHidden">
-	<input type="submit" name="returnToSearch" id="returnToSearch" value="Return to Search!">
-</form>';
-}
-?>
+	<button class="button" type="submit" name="saveRecipeButton" id="saveRecipeButton">Save this Recipe!</button>
+</form>
+
+
 
 <?php
+
 include("dbconn.php");
-$recipeName = $_GET['recipe'];
+
+$recipeURI = $_GET['recipe'];
 echo '<script>
 		$(document).ready(function(){
 				search();
@@ -74,7 +96,7 @@ echo '<script>
 				jsonp: "callback",
 				dataType: "jsonp",
 				data: {
-					q: "' . $recipeName . '",
+					q: "' . $recipeURI . '",
 					from:0,
 					to:1,
 					//calories: "gte 250, lte 500",
@@ -84,10 +106,12 @@ echo '<script>
 				},
 				success: function(callback){
 						$("div.first").replaceWith("<img src=" + callback.hits[0].recipe.image + ">");
-						$("div.second").replaceWith("<h1>" + callback.hits[0].recipe.label + "</h1>");
-						$("div.third").replaceWith("<h1>Calories:" + callback.hits[0].recipe.calories + "</h1>");
+						$("div.second").replaceWith("<h3>" + callback.hits[0].recipe.label + "</h3><br>");
+						var cal = callback.hits[0].recipe.calories;
+						var cal2 = parseInt(cal);
+						$("div.third").replaceWith("<h4>Calories: " + cal2 + "</h4><br>");
 						var len2 = callback.hits[0].recipe.ingredients.length;
-						var s = "<li>";
+						var s = "<li class=\'listItem\'>";
 						var c = "</li>";
 						var str2 = "";
 						for(j=0;j<len2;j++){
@@ -96,16 +120,15 @@ echo '<script>
 							var fin = st.concat(c);
 							str2 = str2.concat(fin);
 						};
-						$("div.fourth").replaceWith("<h1>Ingredients:" + str2 + "</h1>");
-						$("div.fifth").replaceWith("<h1>" + callback.hits[0].recipe.url + "</h1>");
-						$(document.body).append("<iframe class=' . '\'iframe\'' . ' src=" + callback.hits[0].recipe.url + "></iframe>");
+						$("div.fourth").replaceWith("<h4>Ingredients:</h4><br><div class=\'containerX\'><ul id=\'list-unstyled\'" + str2 + "</ul></div>");
+						$("#recipeNameHidden").val(callback.hits[0].recipe.uri);
+						$(document.body).append("<center><iframe class=' . '\'iframe\'' . ' src=" + callback.hits[0].recipe.url + "></iframe></div></center>");
 				}
 			});
 		};
 	</script>'
+
 ?>
-
-
-
+</center>
 </body>
 </html>
